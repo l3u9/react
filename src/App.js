d76299b63d565ee00c521ce133b4d10d64984f76
l3u9 <ksj31292000@gmail.com>
2020-07-04 17:01:02 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -8,9 +8,8 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles';
-import prac
 
-
+test = []
 
 const styles = theme => ({
   root: {
